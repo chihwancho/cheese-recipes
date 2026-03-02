490bdf185cbab886785ada5c6a9dc387c109178e
chihwancho <chihwancho@gmail.com>
2026-03-02 16:08:23 -0600

    Clear API key input on provider change in SettingsModal

--- src/components/ui/SettingsModal.tsx
+++ src/components/ui/SettingsModal.tsx
@@ -37,8 +37,6 @@ export default function SettingsModal({ onClose }: SettingsModalProps) {
 
   const handleProviderChange = (provider: LlmProvider) => {
     updateLlmProvider(provider);
-    setKeyInput('');
-    updateApiKey('');
   };
 
   return (
